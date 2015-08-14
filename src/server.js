44fa5f3c7cb742d68e1d10d5f6986cacdd3c8397
Ethan Resnick <ethan.resnick@gmail.com>
2015-08-15 00:33:57 +0900

    Server.js: remove outdated custom redirects
    (Leaving just one tufte reference as an example)

--- src/server.js
+++ src/server.js
@@ -118,16 +118,8 @@ app.get(/^\/team\/?$/, function(req, res){
   return res.redirect('http://ship.techatnyu.org/#board');
 });
 
-app.get(/^\/master-?class\/?$/, function(req, res){
-  return res.redirect('https://www.eventbrite.com/e/edward-tufte-data-visualization-master-class-tickets-16049397179?discount=NYU');
-});
-
 app.get(/^\/tufte\/?$/, function(req, res){
   return res.redirect('https://www.eventbrite.com/e/edward-tufte-data-visualization-master-class-tickets-16049397179');
-});
-
-app.get(/^\/body-?labs\/?$/, function(req, res){
-  return res.redirect('https://docs.google.com/forms/d/1t1R-GYYTUowAl1gWcIwO3_HrQkx8Qo3bdjFSnFzXKGA/viewform?c=0&w=1');
 });
 
 app.get(/\/job\-board\/?/, function(req, res){
