0aa3bd7122902f0c7c5d7645e86b99c1c929f248
Ethan Resnick <ethan.resnick@gmail.com>
2015-08-26 22:00:48 -0700

    Convert digestSignup component

---
+++ src/public/scripts/components/digestSignup.js
@@ -0,0 +1,30 @@
+define(["flight/component", "mixins"], function(defineComponent, mixins){
+
+  return defineComponent(mixins.managesAnimations, mixins.usesSassVars, function(){
+    this.defaultAttrs({
+      details: '.caption',
+      submit: "[type=submit]",
+      input: "[type=email]"
+    });
+
+    this.showDetails = function(){
+      var details = this.select('details').show().removeClass('hidden');
+      this.trigger('digestDetailsShown', { 'height': details.outerHeight() });
+    };
+
+    this.hideDetails = function(){
+      var details = this.select('details');
+      var height = details.outerHeight();
+
+      details.hide().addClass('hidden');
+      this.trigger('digestDetailsHidden', { 'height': height });
+    };
+
+    return this.after('initialize', function(){
+      this.on(this.select('input'), "focus", this.showDetails);
+      this.on(this.select('input'), 'blur', function(){
+        return setTimeout(this.hideDetails.bind(this), 150);
+      });
+    });
+  });
+});
