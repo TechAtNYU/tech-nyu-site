0a5488732c5519c63b277a132baf4669df57e3c7
Abhi Agarwal <me@abhiagarwal.com>
2016-01-08 23:37:32 +0700

    Adding positions open for 2016

--- src/data/applications.js
+++ src/data/applications.js
@@ -1,66 +1,27 @@
 module.exports = {
   positions: [
     {
-      name: "Design Days Lead",
-      link: "https://docs.google.com/forms/d/1sgX6Yt30J4ISU1fTJ2E11USLAqrUPKkhW3ZDk4IQogc/viewform",
-      desc: 'After its semi-hibernation last year, we&rsquo;re looking for someone to relaunch Design Days in a big way. We&rsquo;ll give the right candidate the platform and complete freedom to make this a spectacular initiative. The ideal candidate loves shows like <a href="http://99percentinvisible.org/">99% Invisible</a>, has a strong opinion on Massimo Vignelli, loves to draw, or is otherwise committed to design. As with any lead position, you must be organized, creative, and a team-player. Before applying, consider checking out <a href="http://www.meetup.com/Product-Design-Workshop">what we&rsquo;ve done in the past</a> to see what Design Days can involve.',
-      priority: true,
-      filled: 0,
-      needed: 1
-    }, {
-      name: "Community Lead",
-      desc: 'The community team puts on 12-15 fun events each semester that make our teams and our larger community feel closer. The ideal candidate is organized, friendly, and excited to get to know our awesome board members on a more personal level.',
-      link: "https://docs.google.com/forms/d/1WnFItJa0YF8e2TDCq7LfwMqvHifTpRDd2tZwnINVRh8/viewform?entry.1605223786&entry.1037077355&entry.388142826&entry.1322754712&entry.125649264=Community+Lead&entry.584945502&entry.940155488&entry.613662354&entry.852593520&entry.213356174&entry.1989471395",
-      priority: false,
-      filled: 0,
-      needed: 1
-    }, {
-      name: "Treasurer",
-      desc: 'The treasurer oversees the Tech@NYU budget, tracking our income and expenses and helping to plan our spending. Candidates should be highly organized and attentive to detail.',
-      link: "https://docs.google.com/forms/d/1WnFItJa0YF8e2TDCq7LfwMqvHifTpRDd2tZwnINVRh8/viewform?entry.1605223786&entry.1037077355&entry.388142826&entry.1322754712&entry.125649264=Treasurer&entry.584945502&entry.940155488&entry.613662354&entry.852593520&entry.213356174&entry.1989471395",
-      priority: false,
-      filled: 1,
-      needed: 1
-    }, {
       name: 'Startup Week Team Member',
-      desc: 'Startup Week is Tech@NYU&rsquo;s flagship event; it&rsquo;s a week-long conference and hackathon that draws 500+ attendees. As a Startup Week Team Member, you&rsquo;ll be responsible for supporting the creation and planning of this remarkable event. If you are organized, a team player, and interested in&mdash;or, even better, well connected to&mdash;the NYC tech scene, this position is for you.',
-      link: 'https://docs.google.com/forms/d/1WnFItJa0YF8e2TDCq7LfwMqvHifTpRDd2tZwnINVRh8/viewform?entry.1605223786&entry.1037077355&entry.388142826&entry.1322754712&entry.125649264=Startup+Week+Team+Member&entry.584945502&entry.940155488&entry.613662354&entry.852593520&entry.213356174&entry.1989471395',
+      desc: 'We\'re looking for someone to help plan our annual marquee event, Startup Week. Startup Week is a weeklong conference with technical, design, and entrepreneurship workshops and hacks. We bring in the best and brightest to share their skills and stories. Past speakers have included Josh Kushner of Oscar, Dennis Crowley of Foursquare, Jennifer Hyman of Rent the Runway, David Tisch of BoxGroup and Spring, and Chris Dixon of Andreessen Horowitz.',
+      roles: ['Work with a team to own 2-3 events of startup week from sourcing speakers to finding venues.', 'Run the show on the day of events along with the help of other team members and volunteers.', 'Coordinate with marketing and business development teams.'],
+      link: 'https://techatnyu.typeform.com/to/b0vcli',
       priority: true,
-      filled: 2,
-      needed: 4
+      filled: 0,
+      needed: 1
     }, {
       name: "Business Development Team Member",
-      desc: 'Our Business Development team works hard to secure the funds that Tec@NYU needs to properly execute its programs. It does this primarily through <a href="http://techatnyu.org/#about">sponsorships with companies</a> that believe in Tech@NYU&rsquo;s vision. The ideal candidate is interested in meeting with companies throughout NYC, is hard-working and ambitious, and a people-person.',
-      link: 'https://docs.google.com/forms/d/1WnFItJa0YF8e2TDCq7LfwMqvHifTpRDd2tZwnINVRh8/viewform?entry.1605223786&entry.1037077355&entry.388142826&entry.1322754712&entry.125649264=Business+Development+Team+Member&entry.584945502&entry.940155488&entry.613662354&entry.852593520&entry.213356174&entry.1989471395',
+      desc: 'Our Biz Dev team is at the core of building professional relationships between Tech@NYU and the greater NYC technology scene. Our Biz Dev team is responsible for helping to coordinate sponsorships, locations for events, and future partnerships with technology companies and startups in the city. Being a member of this team will provide you with access to contacts at some top tech companies, as well as help foster skills for building a professional network.',
+      roles: ['Reach out to local startups and big companies for sponsorship and partnerships for Tech@NYU as a whole and specific events.', 'Take calls, emails and meetings with representatives from current and potential sponsors.', 'Preferably a freshmen or sophomore.'],
+      link: 'https://techatnyu.typeform.com/to/b0vcli',
       priority: true,
-      filled: 2,
-      needed: 4
-    }, {
-      name: 'Design Days Team Member',
-      desc: 'Design Days consist of talks and workshops on design thinking, user research, and advanced topics in digital product design. As a Design Days Team Member, you&rsquo;ll be responsible for supporting the team lead in the creation, planning, and hosting these events throughout the semester. If you are organized, creative, a team player, and love all things design, this is the position for you.',
-      link: 'https://docs.google.com/forms/d/1WnFItJa0YF8e2TDCq7LfwMqvHifTpRDd2tZwnINVRh8/viewform?entry.1605223786&entry.1037077355&entry.388142826&entry.1322754712&entry.125649264=Design+Days+Team+Member&entry.584945502&entry.940155488&entry.613662354&entry.852593520&entry.213356174&entry.1989471395',
-      priority: true,
-      filled: 1,
-      needed: 4
-    }, {
-      name: 'Demo Days Team Member',
-      desc: 'Demo Days is one of Tech@NYU&rsquo;s best attended events, at which students from all over NYC show off hacks or projects they&rsquo;ve been working on. As a Demo Days Team Member, you are responsible for supporting the team lead in the creation, planning, and hosting of these monthly events. The ideal candidate is organized, a team player, and interested in finding students working on cool projects throughout NYC.',
-      link: 'https://docs.google.com/forms/d/1WnFItJa0YF8e2TDCq7LfwMqvHifTpRDd2tZwnINVRh8/viewform?entry.1605223786&entry.1037077355&entry.388142826&entry.1322754712&entry.125649264=Demo+Days+Team+Member%C2%A0&entry.584945502&entry.940155488&entry.613662354&entry.852593520&entry.213356174&entry.1989471395',
-      priority: false,
-      filled: 0,
-      needed: 2
-    }, {
-      name: 'Freshman Circuit Team Member',
-      desc: 'Freshman Circuit builds community amongst our newest NYC tech members&mdash;NYU freshman&mdash;hosting events and tours of NYC startups exclusively for them. As a Freshman Circuit Team Member, your are responsible for supporting the creation, planning, and hosting of these events throughout the semester. If you are organized, a team player, and interested in learning more about the NYC tech scene while helping to foster community amongst freshman, this the is position for you.',
-      link: 'https://docs.google.com/forms/d/1WnFItJa0YF8e2TDCq7LfwMqvHifTpRDd2tZwnINVRh8/viewform?entry.1605223786&entry.1037077355&entry.388142826&entry.1322754712&entry.125649264=Freshman+Circuit+Team+Member&entry.584945502&entry.940155488&entry.613662354&entry.852593520&entry.213356174&entry.1989471395',
-      priority: false,
       filled: 0,
       needed: 1
     }, {
-      name: 'Marketing Team Member',
-      desc: 'We&rsquo;re looking to add two more members to the marketing team, one who will be responsible for social media (Facebook, Twitter, etc.) and another who will be focused on physical marketing campaigns (posters, handouts, mini-events, speaking to classes, etc.). If you have experience with hands on online or physical marketing, this is the position for you.',
-      link: 'https://docs.google.com/forms/d/1WnFItJa0YF8e2TDCq7LfwMqvHifTpRDd2tZwnINVRh8/viewform?entry.1605223786&entry.1037077355&entry.388142826&entry.1322754712&entry.125649264=Marketing+Team+Member&entry.584945502&entry.940155488&entry.613662354&entry.852593520&entry.213356174&entry.1989471395',
-      priority: false,
+      name: 'After Hours Lead',
+      desc: 'After hours is our weekly opportunity for designers, developers, and makers of all sorts coming together to hang out, work on their projects and get to know each other. All experience levels welcome! We\'re looking for someone to help host and organize these weekly gatherings for the duration of the semester.',
+      roles: ['Organize weekly social events, including ordering food, and assigning other executive board members to attend each After Hours of the semester.', 'Collaborate with marketing team to get the word out.', 'Lead events and work to build a community around technology and startups.', 'Evangelize Tech@NYU.', 'Must be able to attend majority of After Hours events each semester.'],
+      link: 'https://techatnyu.typeform.com/to/b0vcli',
+      priority: true,
       filled: 0,
       needed: 1
     }
